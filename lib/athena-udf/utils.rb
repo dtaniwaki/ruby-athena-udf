86b839d4bc0d6c36e21bcc3530db1d28d84fad2d
Daisuke Taniwaki <daisuketaniwaki@gmail.com>
2024-09-03 22:22:58 +0900

    Avoid tmp file interactions

--- lib/athena-udf/utils.rb
+++ lib/athena-udf/utils.rb
@@ -3,13 +3,13 @@
 module AthenaUDF
   module Utils
     def read_record_batches(schema_data, record_batch_data)
-      Tempfile.create do |in_f|
-        in_f.write(schema_data)
-        in_f.write(record_batch_data)
-        in_f.flush
+      buffer = Arrow::ResizableBuffer.new(schema_data.bytes.size + record_batch_data.bytes.size)
+      Arrow::BufferOutputStream.open(buffer) do |output|
+        output.write(schema_data)
+        output.write(record_batch_data)
 
-        Arrow::MemoryMappedInputStream.open(in_f.path) do |inp|
-          reader = Arrow::RecordBatchStreamReader.new(inp)
+        Arrow::BufferInputStream.open(buffer) do |input|
+          reader = Arrow::RecordBatchStreamReader.new(input)
           input_schema = reader.schema
           reader.each do |record_batch|
             yield input_schema, record_batch
@@ -19,56 +19,49 @@ module AthenaUDF
     end
 
     def read_schema(schema_data)
-      # schema_buf = Arrow::Buffer.try_convert(schema_data)
-      Tempfile.create do |f|
-        f.write(schema_data)
-        f.flush
+      buffer = Arrow::ResizableBuffer.new(schema_data.bytes.size)
+      Arrow::BufferOutputStream.open(buffer) do |output|
+        output.write(schema_data)
 
-        Arrow::MemoryMappedInputStream.open(f.path) do |inp|
-          reader = Arrow::RecordBatchStreamReader.new(inp)
+        Arrow::BufferInputStream.open(buffer) do |input|
+          reader = Arrow::RecordBatchStreamReader.new(input)
           reader.schema
         end
       end
     end
 
     def get_schema_bytes(schema, record_batch)
-      Tempfile.create do |f|
-        Arrow::FileOutputStream.open(f.path, false) do |oup|
-          Arrow::RecordBatchStreamWriter.open(oup, schema) do |writer|
-            writer.write_record_batch(record_batch)
-          end
+      buffer = Arrow::ResizableBuffer.new(0)
+      Arrow::BufferOutputStream.open(buffer) do |output|
+        Arrow::RecordBatchStreamWriter.open(output, schema) do |writer|
+          writer.write_record_batch(record_batch)
         end
-        f.flush
 
-        data = File.binread(f.path)
-        start_index = get_record_batch_index(data)
-        data.bytes[4..start_index - 5].pack('C*')
+        bytes = buffer.data.to_s
+        start_index = get_record_batch_index(bytes)
+        bytes[4..start_index - 5]
       end
     end
 
     def get_record_batch_bytes(schema, record_batch)
-      Tempfile.create do |f|
-        Arrow::FileOutputStream.open(f.path, false) do |oup|
-          Arrow::RecordBatchStreamWriter.open(oup, schema) do |writer|
-            writer.write_record_batch(record_batch)
-          end
+      buffer = Arrow::ResizableBuffer.new(0)
+      Arrow::BufferOutputStream.open(buffer) do |output|
+        Arrow::RecordBatchStreamWriter.open(output, schema) do |writer|
+          writer.write_record_batch(record_batch)
         end
-        f.flush
 
-        data = File.binread(f.path)
-        start_index = get_record_batch_index(data)
-        data.bytes[start_index..].pack('C*')
+        bytes = buffer.data.to_s
+        start_index = get_record_batch_index(bytes)
+        bytes[start_index..]
       end
     end
 
-    def get_record_batch_index(raw)
-      size = raw.bytes.size
+    def get_record_batch_index(bytes)
+      size = bytes.size
       found_count = 0
       start_index = 0
       0.upto(size - 4).each do |i|
-        has_ffff = 4.times.all? do |n|
-          raw.bytes[i + n] == 255
-        end
+        has_ffff = bytes.slice(i, 4) == [255, 255, 255, 255]
         found_count += 1 if has_ffff
         if found_count == 2
           start_index = i + 4
